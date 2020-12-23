7bf5b0ec90bce3d3260ed20a3ac28365ee330909
Steve Nield <snield2012@gmail.com>
2020-12-24 07:28:20 +1030

    End of Section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit{
     this.setCurrentUser();   
   }
   setCurrentUser(){
-    const user: User = JSON.parse(localStorage.getItem('user') || "{}");
+    const user: User = JSON.parse(localStorage.getItem('user') );
     this.accountService.setCurrentUser(user);
 
   }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,15 +10,18 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
     @Output() cancelRegister = new EventEmitter();
 model: any = {};
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 register(){
   this.accountService.register(this.model).subscribe(response =>{
-    console.dir("register response = "+ response)
+    console.log("register response = "+ response)
     this.cancel();
-  })
+  }  , error =>{
+      console.log(error);
+      this.toastr.error(error.error);
+    })
 }
 cancel(){
   this.cancelRegister.emit(false);
